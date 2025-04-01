ca179c4d01de6cb6a917f081ac7eeeb14c909d06
Fabrice Cheng <fabrice.cheng@gmail.com>
2025-03-31 21:55:44 -0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ export const metadata: Metadata = {
   title: "Strategic ETH Reserve",
   description:
     "Join the Strategic Ethereum Reserve (SER) movement. Companies pledging to maintain ETH reserves for a sustainable Ethereum ecosystem.",
+  icons: {
+    icon: "/images/strategicethreserve.svg",
+    shortcut: "/images/strategicethreserve.svg",
+  },
 };
 
 export default function RootLayout({
